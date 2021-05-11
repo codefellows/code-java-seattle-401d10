b40ea9153f7132b3dee91c45880aff2f93c8bab7
Nicholas Carignan <nick.carignan@sbcglobal.net>
2021-05-11 16:17:14 -0700

    day 37

---
+++ class-37/demos/TornadoTracker/app/src/main/java/com/amplifyframework/datastore/generated/model/Query.java
@@ -0,0 +1,16 @@
+package com.amplifyframework.datastore.generated.model;
+
+
+import java.util.List;
+import java.util.UUID;
+import java.util.Objects;
+
+import androidx.core.util.ObjectsCompat;
+
+import com.amplifyframework.core.model.Model;
+import com.amplifyframework.core.model.annotations.Index;
+import com.amplifyframework.core.model.annotations.ModelConfig;
+import com.amplifyframework.core.model.annotations.ModelField;
+import com.amplifyframework.core.model.query.predicate.QueryField;
+
+import static com.amplifyframework.core.model.query.predicate.QueryField.field;
